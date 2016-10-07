b92fc10b1f874090c760323637b9096db52508e2
mathiassamyn <mathiassamyn@hotmail.com>
2016-10-07 10:30:43 +0200

    start using bitmap overlay

--- src/application/app/src/main/java/com/example/mathias/iot_parkassist/MainActivity.java
+++ src/application/app/src/main/java/com/example/mathias/iot_parkassist/MainActivity.java
@@ -1,9 +1,17 @@
 package com.example.mathias.iot_parkassist;
 
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +19,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView caravan = (ImageView) findViewById(R.id.caravan);
+        caravan.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+
+                //textView.setText("Touch coordinates : " +
+                  //      String.valueOf(event.getX()) + "x" + String.valueOf(event.getY()));
+                return true;
+            }
+        });
     }
 
     @Override
@@ -33,5 +52,38 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        int x = (int)event.getX();
+        int y = (int)event.getY();
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+            case MotionEvent.ACTION_MOVE:
+
+            case MotionEvent.ACTION_UP:
+        }
+        return false;
+    }
+
+    private void addSensor() {
+        Bitmap bitmap1 = BitmapFactory.decodeResource(getResources(), R.drawable.caravan);
+        Bitmap bitmap2 = BitmapFactory.decodeResource(getResources(), R.drawable.sensor);
+
+        Bitmap resultBitmap = Bitmap.createBitmap(bitmap1.getWidth(), bitmap1.getHeight(), Bitmap.Config.ARGB_8888);
+
+        Canvas c = new Canvas(resultBitmap);
+
+        c.drawBitmap(bitmap1, 0, 0, null);
+
+        Paint p = new Paint();
+        p.setAlpha(127);
+
+        c.drawBitmap(bitmap2, 0, 0, p);
+
+
+
+        // Your final bitmap is resultBitmap
     }
 }
